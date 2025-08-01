dc50a8b1968c1ab467a8ae26d50f05ce3ecfa0af
parkriti1111 <prakritigupta468@gmail.com>
2025-08-01 17:29:16 +0530

    creating the login page

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom';
+import "../../vars.css";
+import PasswordInput from '../../components/Input/PasswordInput';
 
 function Login() {
   return (
     <>
         <Navbar />
+        <div className='flex items-center justify-center mt-28'>
+            <div className='w-96 border border-zinc-300 rounded bg-white px-7 py-10'>
+                <form onSubmit={()=>{}}>
+                    <h4 className='text-2xl mb-7'>Login</h4>
+                    <input type="text" placeholder='Email' className='input-box'/>
+                    <PasswordInput />
+                    <button type="submit" className="btn-primary">Login</button>
+                    <p className='text-sm text-center mt-4'>Not registered yet{" "}
+                        <Link to="/signup" className="font-medium text-[var(--color-primary)] underline">Create an Account</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     </>
   )
 }
