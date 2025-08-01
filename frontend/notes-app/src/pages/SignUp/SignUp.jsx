ca762f905a81564f8e4069b0d1934ff280ebc0b7
parkriti1111 <prakritigupta468@gmail.com>
2025-08-01 20:27:29 +0530

    SignUp page

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <>
+        <Navbar />
+        <div className='flex items-center justify-center mt-28'>
+            <div className='w-96 border border-zinc-300 rounded bg-white px-7 py-10'>
+                <form onSubmit={handleSignUp}>
+                    <h4 className='text-2xl mb-7'>Login</h4>
+                </form>
+            </div>
+        </div>
+    </>
   )
 }
 
