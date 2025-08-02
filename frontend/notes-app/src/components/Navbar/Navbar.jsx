5c90601929a72ba39da4475d846dfdea70c1e06e
parkriti1111 <prakritigupta468@gmail.com>
2025-08-02 11:33:36 +0530

    SearchBar

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
 import { useNavigate } from 'react-router-dom'
+import SearchBar from '../Searchbar/SearchBar'
 
 const Navbar = () => {
 
@@ -14,6 +15,7 @@ const Navbar = () => {
     <>
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+            <SearchBar />
             <ProfileInfo  onLogout={onLogout}/>
         </div>
     </>
