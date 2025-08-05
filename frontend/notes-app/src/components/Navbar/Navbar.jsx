190e3f9dcc42d0425a5fec57a47e72256bea47a8
parkriti1111 <prakritigupta468@gmail.com>
2025-08-05 18:47:30 +0530

    addEditButton

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
 import { useNavigate } from 'react-router-dom'
 import SearchBar from '../Searchbar/SearchBar'
 
 const Navbar = () => {
 
+  const [searchQuery , setSearchQuery] = useState("");
+
+
   const navigate = useNavigate()
   
+  const handleSearch = () => {
+
+  }
+
+  const onClearSearch = ()=>{
+    setSearchQuery("");
+  }
 
   const onLogout =() =>{
     navigate('/login');
@@ -15,7 +25,15 @@ const Navbar = () => {
     <>
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
-            <SearchBar />
+            <SearchBar 
+              value={searchQuery}
+              onChange = {({target})=>{
+              setSearchQuery(target.value);
+            }}
+              handleSearch = {handleSearch}
+              onClearSearch = {onClearSearch}
+              
+            />
             <ProfileInfo  onLogout={onLogout}/>
         </div>
     </>
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
+import NoteCard from '../../components/Cards/NoteCard';
+import {MdAdd} from "react-icons/md";
+import AddEditNotes from './AddEditNotes';
+import Modal from 'react-modal';
+
 function Home() {
+
+const [openAddEditModal, setOpenAddEditModal] = useState({
+  isShown: false,
+  type : "add",
+  data : null,
+});
+
   return (
     <>
       <Navbar />
+      <div className='container mx-auto'>
+
+      <div className="grid grid-cols-3 gap-4 mt-8 mr-12 ml-12">
+        <NoteCard  title="Meeting on 6th August" 
+        date="2nd August 2025" 
+        content="Meeting on 6th August"
+        tags="#Meeting"
+        isPinned={true}
+        onEdit={()=>{
+          
+        }}
+        onDelete={()=>{
+          
+        }}
+        onPinNote={()=>{
+          
+        }}
+      />
+      </div>
+    </div>
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-[var(--color-primary)] hover:bg-blue-600 absolute right-10 bottom-10' 
+      onClick={()=>{
+        setOpenAddEditModal({isShown: true,type: "add",data: null});
+      }} >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <Modal 
+        isOpen = {openAddEditModal.isShown}
+        onRequestClose = {()=>{}}
+        style = {{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel = ""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      > <AddEditNotes />
+      </Modal>
+
+      
+
+     
     </>
   )
 }
 
-export default Home
+export default Home;
