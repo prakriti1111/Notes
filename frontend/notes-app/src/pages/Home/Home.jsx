4da7991e5fe3f1dcce25d8206939202cb5bd16f1
parkriti1111 <prakritigupta468@gmail.com>
2025-08-25 22:46:01 +0530

    created frontend for login, signup,and homepage

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -18,23 +18,23 @@ const [openAddEditModal, setOpenAddEditModal] = useState({
       <Navbar />
       <div className='container mx-auto'>
 
-      <div className="grid grid-cols-3 gap-4 mt-8 mr-12 ml-12">
-        <NoteCard  title="Meeting on 6th August" 
-        date="2nd August 2025" 
-        content="Meeting on 6th August"
-        tags="#Meeting"
-        isPinned={true}
-        onEdit={()=>{
-          
-        }}
-        onDelete={()=>{
-          
-        }}
-        onPinNote={()=>{
-          
-        }}
-      />
-      </div>
+        <div className="grid grid-cols-3 gap-4 mt-8 mr-12 ml-12">
+            <NoteCard  title="Meeting on 6th August" 
+            date="2nd August 2025" 
+            content="Meeting on 6th August"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={()=>{
+              
+            }}
+            onDelete={()=>{
+              
+            }}
+            onPinNote={()=>{
+              
+            }}
+          />
+        </div>
     </div>
       <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-[var(--color-primary)] hover:bg-blue-600 absolute right-10 bottom-10' 
       onClick={()=>{
@@ -43,18 +43,26 @@ const [openAddEditModal, setOpenAddEditModal] = useState({
         <MdAdd className="text-[32px] text-white" />
       </button>
 
-      <Modal 
-        isOpen = {openAddEditModal.isShown}
-        onRequestClose = {()=>{}}
-        style = {{
-          overlay: {
-            backgroundColor: "rgba(0,0,0,0.2)",
-          },
-        }}
-        contentLabel = ""
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
-      > <AddEditNotes />
-      </Modal>
+      
+            <Modal 
+            isOpen = {openAddEditModal.isShown}
+            onRequestClose = {()=>{}}
+            style = {{
+              overlay: {
+                backgroundColor: "rgba(0,0,0,0.2)",
+              },
+            }}
+            contentLabel = ""
+            className="w-[42%] max-h-[85%] bg-white rounded-md mx-auto mt-14 p-5 "
+          > <AddEditNotes 
+              type = {openAddEditModal.type}
+              noteData = {openAddEditModal.data}
+            onClose={()=>{
+              setOpenAddEditModal({isShown: false, type: "add", data : null});
+            }}
+          />
+          </Modal>
+      
 
       
 
