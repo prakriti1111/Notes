deeb2a92e569c8e20d934bcec24322b29c5c1451
parkriti1111 <prakritigupta468@gmail.com>
2025-09-02 12:24:01 +0530

    Backend is ready

---
+++ backend/models/note.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema  = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: {type: String, required: true},
+    content:{
+        type: String ,
+        required: true
+    },
+    tags: {
+        type : [String] ,
+        default: []
+    },
+    isPinned: {
+        type: Boolean,
+        default: false
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        
+    },
+});
+
+module.exports = mongoose.model("Note",noteSchema);
